033c6f62aa3566601a1c17274fdbfc9d0e45d06f
wmcentire <wmcentire@student.neumont.edu>
2023-10-25 22:03:11 -0600

    uh more stuff

--- Scene_Manager.cs
+++ Scene_Manager.cs
@@ -8,7 +8,20 @@ public partial class Scene_Manager : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-
+		int index = 0;
+		foreach(var item in game_manager.Players)
+		{
+			level currentPlayer = playerScene.Instantiate<level>();
+			currentPlayer.Name = item.name;
+			AddChild(currentPlayer);
+			foreach (Node3D spawnPoint in GetTree().GetNodesInGroup("PlayerSpawnPoints"))
+			{
+				if(int.Parse(spawnPoint.Name) == index)
+				{
+					currentPlayer.GlobalPosition = spawnPoint.GlobalPosition;
+				}
+			}
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
